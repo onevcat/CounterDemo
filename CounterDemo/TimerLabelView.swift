0b6c6433afb216bbdad57965e799a94c296b60a5
onevcat <onevcat@gmail.com>
2022-01-24 14:13:01 +0900

    Add basic timer label view

---
+++ CounterDemo/TimerLabelView.swift
@@ -0,0 +1,93 @@
+//
+//  TimerLabelView.swift
+//  CounterDemo
+//
+//  Created by Wang Wei on 2021/12/19.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct TimerState: Equatable {
+  var started: Date? = nil
+  var duration: TimeInterval = 0
+}
+
+enum TimerAction {
+  case start
+  case stop
+  case timeUpdated
+}
+
+struct TimerEnvironment {
+  var date: () -> Date
+  var mainQueue: AnySchedulerOf<DispatchQueue>
+  
+  static var live: TimerEnvironment {
+    .init(
+      date: Date.init,
+      mainQueue: .main
+    )
+  }
+}
+
+let timerReducer = Reducer<TimerState, TimerAction, TimerEnvironment> {
+  state, action, environment in
+  
+  struct TimerId: Hashable {}
+  
+  switch action {
+  case .start:
+    if state.started == nil {
+      state.started = environment.date()
+    }
+    return Effect.timer(
+      id: TimerId(),
+      every: .milliseconds(10),
+      tolerance: .zero,
+      on: environment.mainQueue
+    ).map { time -> TimerAction in
+      print(time)
+      return TimerAction.timeUpdated
+    }
+  case .timeUpdated:
+    guard let started = state.started else {
+      return .none
+    }
+    state.duration += 0.01
+    return .none
+  case .stop:
+    return .cancel(id: TimerId())
+  }
+}
+
+
+struct TimerLabelView: View {
+  let store: Store<TimerState, TimerAction>
+  var body: some View {
+    WithViewStore(store) { viewStore in
+      VStack(alignment: .leading) {
+        Label(viewStore.started == nil ? "-" : "\(viewStore.started!.formatted(date: .omitted, time: .standard))", systemImage: "clock")
+        Label("\(viewStore.duration, format: .number)s", systemImage: "timer")
+      }
+    }
+  }
+}
+
+struct TimerLabelView_Previews: PreviewProvider {
+  static let store1 = Store(initialState: .init(), reducer: timerReducer, environment: .live)
+  static let store2 = Store(initialState: .init(), reducer: timerReducer, environment: .live)
+  static var previews: some View {
+    VStack {
+      WithViewStore(store1) { viewStore in
+        VStack {
+          TimerLabelView(store: store1)
+          HStack {
+            Button("Start") { viewStore.send(.start) }
+            Button("Stop") { viewStore.send(.stop) }
+          }.padding()
+        }
+      }
+    }
+  }
+}
